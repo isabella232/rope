3d81234c6b7e1c5808a767d9fcc44ce3994734ce
Gokmen Goksel <gokmen@goksel.me>
2017-08-31 23:33:57 -0700

    Nodes: JS: better handling for browser/node.js

--- nodes/js/rope-node.js
+++ nodes/js/rope-node.js
@@ -1,29 +1,37 @@
+// Rope Gateway
 const ROPELIVE = 'https://secure.rope.live'
+
 // Check for browser
 var window = window || {}
-const inBrowser = !!window.Kite
-if (inBrowser)
-  process = { env: { ROPEHOST: ROPELIVE, ROPEDEBUG: Kite.DebugLevel.INFO } }
 
 // Get kite.js
 const Kite = window.Kite || require('kite.js').Kite
+const inBrowser = !!window.Kite
+
+// Create dummy process
+if (inBrowser) {
+  process = { env: { ROPEHOST: ROPELIVE, ROPEDEBUG: Kite.DebugLevel.INFO } }
+  document.body.appendChild(
+    document.createTextNode('Rope-Node-JS: Check console logs for details.')
+  )
+}
 
 // Defaults for Kite instance
-const AUTO_RECONNECT = true
-const LOG_LEVEL = process.env.ROPEDEBUG || Kite.DebugLevel.INFO
 const NAME = 'rope-node-js'
 const ROPEHOST = process.env.ROPEHOST || ROPELIVE
+const LOG_LEVEL = process.env.ROPEDEBUG || Kite.DebugLevel.INFO
 const ENVIRONMENT = inBrowser ? 'Browser' : 'Node.js ' + process.version
+const AUTO_RECONNECT = true
 
 // The rope-node api
 const api = {
-  'rope.identified': function(res) {
+  'rope.identified': res => {
     if (res.environment) {
       kite.environment = res.environment
     }
     kite.emit('info', `Identified as ${res.id} now!`)
   },
-  'rope.identify': function(id, callback) {
+  'rope.identify': (id, callback) => {
     kite.emit('info', 'Identify requested!')
     const kiteInfo = {
       api: Object.keys(api),
@@ -35,10 +43,10 @@ const api = {
     if (inBrowser) kiteInfo.useragent = navigator.userAgent
     callback(null, kiteInfo)
   },
-  'rope.notify': function(notification) {
+  'rope.notify': notification => {
     console.log('Notification:', notification)
   },
-  square: function(number, callback) {
+  square: (number, callback) => {
     callback(null, number * number)
   },
 }
