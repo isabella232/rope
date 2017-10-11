52e6cbf7d8adc56540aeb61948d522bfc39e17ff
Gokmen Goksel <gokmen@goksel.me>
2017-10-11 00:24:54 -0700

    RopeServer initial test

--- src/rope.test.js
+++ src/rope.test.js
@@ -1,6 +1,17 @@
-import rope from './rope'
+import RopeServer from './rope'
 
-describe('rope', () =>
-  it('should provide rope.Server', () => {
-    expect(rope.Server).toBeDefined()
-  }))
+describe('rope', () => {
+  it('should provide RopeServer', () => {
+    expect(RopeServer).toBeDefined()
+  })
+  describe('RopeServer', () => {
+    it('should initialize without an error', done => {
+      let server = new RopeServer()
+      server.listen()
+      setTimeout(_ => {
+        server.close()
+        done()
+      }, 500)
+    })
+  })
+})
